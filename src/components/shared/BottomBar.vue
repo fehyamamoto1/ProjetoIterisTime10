a0391dcf02a9f43f6007749d2a2eac86b7a34485
fehyamamoto1 <felipeyamamoto46@gmail.com>
2021-03-13 12:16:40 -0300

    Adição da BottomBar

---
+++ src/components/shared/BottomBar.vue
@@ -0,0 +1,64 @@
+<template>
+     <div class="app-bottom-bar">
+         <v-app-bar hide-on-scroll color="white" app dark>
+            <v-spacer />
+            <v-toolbar-title></v-toolbar-title>
+            <v-spacer />
+        </v-app-bar>
+        <v-bottom-navigation :value="value" color="#6d6d6d" app grow>
+            <router-link to='/'>
+                <v-btn>
+                    <span>Início</span>
+
+                    <v-icon>mdi-home</v-icon>
+                </v-btn>
+            </router-link>
+            
+            <router-link to ='/'>
+                <v-btn>
+                    <span>Locais</span>
+                    <v-icon>mdi-office-building-marker-outline</v-icon>
+                </v-btn>
+            </router-link>
+
+            <router-link to='/login'>
+                <v-btn>
+                    <span>Login</span>
+
+                    <v-icon>mdi-account</v-icon>
+                </v-btn>
+            </router-link>
+
+            <router-link to='/'>
+                <v-btn>
+                    <span>Contato</span>
+
+                    <v-icon>mdi-account</v-icon>
+                </v-btn>
+            </router-link>
+
+        </v-bottom-navigation>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "BottomBar",
+    data() { 
+        return {
+            value: 0
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .app-bottom-bar a {
+            text-decoration: none;
+            display: flex;
+            flex-grow: 1;
+            max-width: 90px;   
+            height: 100%;  
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Registro from '../views/Registro.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
+ 
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/registro',
+    name: 'Registro',
+    component: Registro
   }
+
+
 ]
 
 const router = new VueRouter({
